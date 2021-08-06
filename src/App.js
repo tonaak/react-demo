3d19db3c203adacb57304265f97dad8fa446989b
tonaak <toannguyenitit@gmail.com>
2021-08-06 12:18:51 +0700

    Save todoItems state to local storage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import './components/footer.css';
-import React, { Component } from 'react';
-import TodoItem from './components/TodoItem';
-import checkAll from './img/down-arrow.png';
-var classNames = require('classnames');
+import "./App.css";
+import "./components/footer.css";
+import React, { Component } from "react";
+import TodoItem from "./components/TodoItem";
+import checkAll from "./img/down-arrow.png";
+var classNames = require("classnames");
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      newItem: '',
-      currentFilter: 'all',
-      todoItems: [
-        {title: 'Đi ăn bánh', isComplete: true},
-        {title: 'Đi bắn bi', isComplete: true},
-        {title: 'Đi đá banh', isComplete: false}
-      ]
+      newItem: "",
+      currentFilter: "all",
+      todoItems:
+        localStorage.getItem("list") === null
+          ? []
+          : JSON.parse(localStorage.getItem("list"))
     };
 
     this.onKeyUp = this.onKeyUp.bind(this);
@@ -23,6 +22,11 @@ class App extends Component {
     this.checkAllItem = this.checkAllItem.bind(this);
     this.filterItem = this.filterItem.bind(this);
     this.clearCompleted = this.clearCompleted.bind(this);
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem("list", JSON.stringify(this.state.todoItems));
+    console.log(localStorage.getItem("list"));
   }
 
   onItemClick(item) {
@@ -33,7 +37,7 @@ class App extends Component {
       this.setState({
         todoItems: [
           ...todoItems.slice(0, index),
-          {...item, isComplete: !isComplete},
+          { ...item, isComplete: !isComplete },
           ...todoItems.slice(index + 1)
         ]
       });
@@ -45,67 +49,58 @@ class App extends Component {
       const { todoItems } = this.state;
       const index = todoItems.indexOf(item);
       this.setState({
-        todoItems: [
-          ...todoItems.slice(0, index),
-          ...todoItems.slice(index + 1)
-        ]
+        todoItems: [...todoItems.slice(0, index), ...todoItems.slice(index + 1)]
       });
     };
   }
 
   clearCompleted() {
     const { todoItems } = this.state;
-    let itemLeft = todoItems.filter(item => !item.isComplete);
+    let itemLeft = todoItems.filter((item) => !item.isComplete);
     this.setState({
-      todoItems: [
-        ...itemLeft
-      ]
+      todoItems: [...itemLeft]
     });
   }
 
   onKeyUp(event) {
     const { todoItems } = this.state;
     let text = event.target.value;
-    if(!text || text.trim() === '')
-      return;
-    
-    if(event.keyCode === 13){
+    if (!text || text.trim() === "") return;
+
+    if (event.keyCode === 13) {
       this.setState({
-        todoItems: [
-          {title: text, isComplete: false},
-          ...todoItems
-        ],
-        newItem: ''
+        todoItems: [{ title: text, isComplete: false }, ...todoItems],
+        newItem: ""
       });
     }
   }
 
   onChange(event) {
     this.setState({
-        newItem: event.target.value
-      });
+      newItem: event.target.value
+    });
   }
 
   checkAllItem() {
     let todoItems = this.state.todoItems;
     let completedCount = 0;
 
-    for(let item of todoItems) {
-      if(item.isComplete) completedCount++;
+    for (let item of todoItems) {
+      if (item.isComplete) completedCount++;
     }
 
-    this.setState(prevState => ({
-          todoItems: prevState.todoItems.map(
-            item => completedCount === todoItems.length ? 
-            { ...item, isComplete: false } : { ...item, isComplete: true }
-          )           
-        }));
-
+    this.setState((prevState) => ({
+      todoItems: prevState.todoItems.map((item) =>
+        completedCount === todoItems.length
+          ? { ...item, isComplete: false }
+          : { ...item, isComplete: true }
+      )
+    }));
   }
 
   filterItem(event) {
     this.setState({
-      currentFilter: event.target.className.split(' ')[0]
+      currentFilter: event.target.className.split(" ")[0]
     });
   }
 
@@ -114,45 +109,71 @@ class App extends Component {
     let items = todoItems;
     let itemLeft = 0;
 
-    if(currentFilter === 'active')
-      items = todoItems.filter(item => !item.isComplete);
-    if(currentFilter === 'completed')
-      items = todoItems.filter(item => item.isComplete);
-    
-    for(let item of todoItems)
-      if(!item.isComplete) itemLeft++;
+    if (currentFilter === "active")
+      items = todoItems.filter((item) => !item.isComplete);
+    if (currentFilter === "completed")
+      items = todoItems.filter((item) => item.isComplete);
+
+    for (let item of todoItems) if (!item.isComplete) itemLeft++;
 
     return (
-    <div className="App"> 
-      <div className="Header">
-        <img src={checkAll} onClick={this.checkAllItem} />
-        <input type="text" 
-          placeholder="Add an item" 
-          onKeyUp={this.onKeyUp} 
-          onChange={this.onChange}
-          value={newItem}>
-        </input>
-      </div>
-      {items.length > 0 && items.map((item, index) =>     
-        <TodoItem key={index} 
-                  item={item} 
-                  onClick={this.onItemClick(item)} 
-                  deleteItem={this.deleteItem(item)} />
-        )}
+      <div className="App">
+        <div className="Header">
+          <img src={checkAll} onClick={this.checkAllItem} alt="" />
+          <input
+            type="text"
+            placeholder="Add an item"
+            onKeyUp={this.onKeyUp}
+            onChange={this.onChange}
+            value={newItem}
+          ></input>
+        </div>
+        {items.length > 0 &&
+          items.map((item, index) => (
+            <TodoItem
+              key={index}
+              item={item}
+              onClick={this.onItemClick(item)}
+              deleteItem={this.deleteItem(item)}
+            />
+          ))}
 
-      {items.length === 0 && 'Nothing here'}
-      <div className="footer">
-        <div className="itemLeft">{itemLeft} item left</div>
-        <ul className="filter">
-          <li className={ classNames('all', {'selected': currentFilter==='all'}) } onClick={this.filterItem}>All</li>
-          <li className={ classNames('active', {'selected': currentFilter==='active'}) } onClick={this.filterItem}>Active</li>
-          <li className={ classNames('completed', {'selected': currentFilter==='completed'}) } onClick={this.filterItem}>Completed</li>
-        </ul>
-        <div className="clearCompleted" onClick={this.clearCompleted}>Clear completed</div>
+        {items.length === 0 && "Nothing here"}
+        <div className="footer">
+          <div className="itemLeft">{itemLeft} item left</div>
+          <ul className="filter">
+            <li
+              className={classNames("all", {
+                selected: currentFilter === "all"
+              })}
+              onClick={this.filterItem}
+            >
+              All
+            </li>
+            <li
+              className={classNames("active", {
+                selected: currentFilter === "active"
+              })}
+              onClick={this.filterItem}
+            >
+              Active
+            </li>
+            <li
+              className={classNames("completed", {
+                selected: currentFilter === "completed"
+              })}
+              onClick={this.filterItem}
+            >
+              Completed
+            </li>
+          </ul>
+          <div className="clearCompleted" onClick={this.clearCompleted}>
+            Clear completed
+          </div>
+        </div>
       </div>
-    </div>
     );
-  }  
+  }
 }
 
 export default App;
